be311e312fb88e9ea9f1ec2724fedb874a517c91
JerryLin <ads079921@hotmail.com>
2024-12-08 01:02:20 +0800

    6-2 answer

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -286,6 +286,12 @@ order by 經驗年數 DESC;
 
 -- 6-2 查詢：查詢每種專長的教練數量，並只列出教練數量最多的專長（需使用 group by, inner join 與 order by 與 limit 語法）
 -- 顯示須包含以下欄位： 專長名稱, coach_total
+SELECT s.name AS 專長名稱,count( cls.coach_id) AS coach_total FROM "COACH" AS c
+INNER JOIN "USER" u ON c.user_id = u.id
+INNER JOIN "COACH_LINK_SKILL" cls ON c.id = cls.coach_id
+INNER JOIN "SKILL" s ON cls.skill_id = s.id
+group by 專長名稱 order by coach_total desc limit 1;
+
 
 -- 6-3. 查詢：計算 11 月份組合包方案的銷售數量
 -- 顯示須包含以下欄位： 組合包方案名稱, 銷售數量
