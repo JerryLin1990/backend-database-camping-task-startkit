7848e55855a87714a60ee5f141bdf95a5604615a
JerryLin <ads079921@hotmail.com>
2024-12-08 01:12:14 +0800

    6-4 answer

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -302,6 +302,8 @@ Group BY (組合包方案名稱);
 
 -- 6-4. 查詢：計算 11 月份總營收（使用 purchase_at 欄位統計）
 -- 顯示須包含以下欄位： 總營收
+SELECT SUM(price_paid) 總營收 FROM "CREDIT_PURCHASE" WHERE purchase_at BETWEEN '2024-12-01' AND '2024-12-31';
+
 
 -- 6-5. 查詢：計算 11 月份有預約課程的會員人數（需使用 Distinct，並用 created_at 和 status 欄位統計）
 -- 顯示須包含以下欄位： 預約會員人數
