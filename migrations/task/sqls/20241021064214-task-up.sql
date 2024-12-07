70874fdd47b1e387414a9a1911adf7f66d9060de
JerryLin <ads079921@hotmail.com>
2024-12-07 23:53:02 +0800

    5-2 answer

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -222,6 +222,13 @@ AND course_id = (SELECT id FROM "COURSE" WHERE user_id=(SELECT id FROM "USER" WH
     -- 1. 預約人設為`王小明`
     -- 2. 預約時間`booking_at` 設為2024-11-24 17:10:25
     -- 3. 狀態`status` 設定為即將授課
+INSERT INTO "COURSE_BOOKING"(user_id, course_id, booking_at, status)
+VALUES
+((SELECT id FROM "USER" WHERE name = '王小明'), 
+ (SELECT id FROM "COURSE" WHERE user_id=(SELECT id FROM "USER" WHERE name = '李燕容')), 
+ '2024-11-24 17:10:25', 
+ '即將授課');
+
 
 -- 5-4. 查詢：取得王小明所有的預約紀錄，包含取消預約的紀錄
 
